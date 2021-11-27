28d81d081c96c99c485dc2dd39238dd89b2aa549
Burak Büyükyüksel <buyukyukselburak@gmail.com>
2021-11-27 13:38:36 +0300

    markdown files can serve

--- lessons/views.py
+++ lessons/views.py
@@ -1,12 +1,21 @@
+import markdown
+import codecs
+
 from django.shortcuts import render
 
 from lessons.models import Lesson
 # Create your views here.
 def lesson(request, pk):
+    mdcontent = None
     lesson = Lesson.objects.filter(pk=pk)[0]
+
+    if lesson.content_path:
+        with codecs.open(lesson.content_path, 'r', 'utf-8') as f:
+            mdcontent = markdown.markdown(f.read(), extensions=['fenced_code', 'codehilite'])
 
     context = {
         'lesson' : lesson,
+        'markdown' : mdcontent,
     }
 
     return render(request, 'lessons/lesson.html', context)
